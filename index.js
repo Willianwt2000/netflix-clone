5c28c261acd1a1322aa0221db010eb1948ed3733
willianwt2000 <willianmetelien@gmail.com>
2023-10-12 23:07:26 -0400

    agregando index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+// let elemento;
+
+// elemento = document;
+// elemento = document.head
+// elemento = document.title
+// console.log(elemento);
+
+// //window
+
+// console.log(window)
+
+
+const elemento = document.querySelector('h1')
+const titulo = document.querySelector('title')
+console.log(elemento)
+console.log(titulo)
+
+const style = document.styleSheets;
+const link = document.links;
+console.log(link)
+console.log(style)
